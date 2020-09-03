c7c13f0dc66c4d60b46ebd7277c5e58086fdb4b5
Sonny Vagn Andersen <20115032@post.au.dk>
2020-09-03 11:55:52 +0200

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private Calculator uut;
+        private double _result;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+            _result = 0;
+        }
+
+        [TestCase(5,13,18)]
+        [TestCase(-5,5,0)]
+        [TestCase(5.4, 13.8,19.2)]
+        [TestCase(7, 0, 7)]
+        public void Add_TwoNumbers_ReturnCorrectResult(double firstNumber, double secondNumber, double result)
+        {
+            _result = uut.Add(firstNumber, secondNumber);
+
+            Assert.That(_result, Is.EqualTo(result).Within(0.005));
+        }
+
+        [TestCase(5, 13, -8)]
+        [TestCase(-5, 5, -10)]
+        [TestCase(5.4, 13.8, -8.4)]
+        [TestCase(7, 0, 7)]
+        public void Subtract_TwoNumbers_ReturnCorrectResult(double firstNumber, double secondNumber, double result)
+        {
+            _result = uut.Subtract(firstNumber, secondNumber);
+
+            Assert.That(_result, Is.EqualTo(result).Within(0.005));
+        }
+
+        [TestCase(5, 13, 65)]
+        [TestCase(-5, 5, -25)]
+        [TestCase(5.4, 13.8, 74.52)]
+        [TestCase(7, 0, 0)]
+        public void Multiply_TwoNumbers_ReturnCorrectResult(double firstNumber, double secondNumber, double result)
+        {
+            _result = uut.Multiply(firstNumber, secondNumber);
+
+            Assert.That(_result, Is.EqualTo(result).Within(0.005));
+        }
+
+        [TestCase(5, 3, 125)]
+        [TestCase(-5, 5, -3125)]
+        [TestCase(5.4, 3.8, 606.8709)]
+        [TestCase(7, 0, 1)]
+        [TestCase(0, 7, 0)]
+        public void Power_TwoNumbers_ReturnCorrectResult(double firstNumber, double secondNumber, double result)
+        {
+            _result = uut.Power(firstNumber, secondNumber);
+
+            Assert.That(_result, Is.EqualTo(result).Within(0.005));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            var calc = new Calculator();
+
+            result = calc.Add(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Add(-3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Subtract(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Multiply(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Power(3, 5);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
