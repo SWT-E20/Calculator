b26e2c04d7c8ec9deff5a2b4a1445b000356300c
mvraa <40834090+mvraa@users.noreply.github.com>
2020-09-10 13:06:43 +0200

    test: push unit test med fejl

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -27,7 +27,7 @@ namespace Calculator.Test.Unit
             _result = uut.Add(firstNumber, secondNumber);
 
             Assert.That(_result, Is.EqualTo(result).Within(0.005));
-            
+
             // alternate way:
             Assert.AreEqual(_result, result, 0.005);
         }
@@ -81,7 +81,7 @@ namespace Calculator.Test.Unit
         [TestCase(10, 5, 10, 2, 2, 625)]
         [TestCase(10, 5, 100, 100, 5, 3125)]
         [TestCase(2.5, 3.3, 1.6, 8, -2, 39.0625)]
-        [TestCase(2, 1, 0, 8, 2, 0)]
+        [TestCase(2, 1, 0, 8, 2, 2)]
         public void Accumulate_MultipleOperations_ReturnCorrectResult(double addNum, double subNum, double mulNum, double divNum, double powNum, double result)
         {
             uut.Add(addNum);
