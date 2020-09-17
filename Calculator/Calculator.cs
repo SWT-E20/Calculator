a72b2dcb7eb045092010755b38d00d12462dc1ec
mvraa <40834090+mvraa@users.noreply.github.com>
2020-09-17 17:07:57 +0200

    Revert "test"
    This reverts commit f2683554352fe15f66a635d7189771d2c6fae9bf.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Accumulator{ get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
+
+        public double Add(double a, double b)
+        {
+            Accumulator = a + b;
+            return Accumulator;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            Accumulator = a - b;
+            return Accumulator;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            Accumulator = a * b;
+            return Accumulator;
+        }
+
+        public double Power(double x, double exp)
+        {
+            Accumulator = Math.Pow(x, exp);
+            return Accumulator;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Error: Division by zero!");
+            }
+
+            Accumulator = dividend / divisor;
+            return Accumulator;
+        }
+
+        public double Add(double addend)
+        {
+            Accumulator += addend;
+            return Accumulator;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            Accumulator -= subtractor;
+            return Accumulator;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            Accumulator *= multiplier;
+            return Accumulator;
+        }
+
+        public double Divide(double divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Error: Division by zero!");
+            }
+            Accumulator /= divisor;
+            return Accumulator;
+        }
+
+        public double Power(double exponent)
+        {
+            Accumulator = Math.Pow(Accumulator, exponent);
+            return Accumulator;
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            var calc = new Calculator();
+
+            result = calc.Add(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Add(-3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Subtract(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Multiply(3, 5);
+            Console.WriteLine(result);
+
+            result = calc.Multiply(-2, -2);
+            Console.WriteLine(result);
+
+            result = calc.Power(3, 5);
+            Console.WriteLine(result);
+
+            /////////// extended ////////////
+            Console.WriteLine("////////extended////////");
+
+            result = calc.Divide(10, 2);
+            Console.WriteLine(result);
+
+            result = calc.Divide(10, 0);
+            Console.WriteLine(result);
+
+            result = calc.Add(10);
+            Console.WriteLine(result);
+
+            result = calc.Subtract(5);
+            Console.WriteLine(result);
+
+            result = calc.Multiply(10);
+            Console.WriteLine(result);
+
+            result = calc.Divide(2);
+            Console.WriteLine(result);
+
+            result = calc.Power(2);
+            Console.WriteLine(result);
+
+            calc.Clear();
+
+            result = calc.Add(10);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
